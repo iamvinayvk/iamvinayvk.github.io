0e886af2e156d3b533051b6c310b15e373884782
Vinay Kushwaha <iamvinayvk@gmail.com>
2020-05-12 00:34:46 +0530

    added type effect

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-import TypeIt from "typeit";
+var m="HI I AM VINAY";
+var next_main="I AM A WEBDEVELOPER";
+var speed=100;
+var i=0;
 
-new TypeIt('.type-animate',{
-	strings:"This element will be typed"
-}).go();
+window.onload=function types()
+{ 
+
+  if(i<m.length)
+  { console.log(m.charAt(i));
+    document.getElementById('name').innerHTML+=m.charAt(i);
+    i++;
+    setTimeout(types,speed);
+  }
+}
+window.onload=function types()
+{ 
+
+  if(i<next_main.length)
+  { console.log(next_main.charAt(i));
+    document.getElementById('name').innerHTML+=next_main.charAt(i);
+    i++;
+    setTimeout(types,speed);
+  }
+}
